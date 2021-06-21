45c8c5d0334bc0c089d6c2e4be10ea477b06822a
Kellie <76688880+Keloxide@users.noreply.github.com>
2021-06-21 14:11:08 -0600

    Add files via upload
    initial commit

---
+++ autos.js
@@ -0,0 +1,46 @@
+
+
+/* FORD JS */
+/*
+const sort = document.querySelector(".ford-sort i");
+const sortUl = document.querySelector(".ford-sort-ul");
+sort.addEventListener("click", () => {
+  if(sortUl.style.display == "none" ){
+    sortUl.style.display = "block";
+  } else {
+    sortUl.style.display = "none";
+  }
+});
+*/
+
+// alert("Te?sla js");
+
+/* TESLA JS */
+// var slideIndex = 1;
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (let i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   for (let i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+// }
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// showSlides(slideIndex);
+
